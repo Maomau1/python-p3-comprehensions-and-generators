8a7a1b77b879a50b74d83523d5bb564bf30adaba
maurice diong <mdiong00@gmail.com>
2024-01-17 14:41:16 -0500

    list comprehension is quite a useful tool

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,9 @@
 
 def return_evens(num_list):
     pass
+    return [item for item in num_list if item % 2 == 0]
+    
 
 def make_exclamation(sentence_list):
     pass
+    return [f"{item}!" for item in sentence_list]
